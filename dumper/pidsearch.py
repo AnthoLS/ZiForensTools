9ff513850406ff7ea9abee17efaafec8ed40588d
AnthoLS <64089132+AnthoLS@users.noreply.github.com>
2026-01-24 12:50:43 +0100

    Pid searcher

---
+++ dumper/pidsearch.py
@@ -0,0 +1,15 @@
+import psutil
+
+def search_proc(target):
+    # On liste les process exisants dans un dictionnaire
+    # On leur associe le nom du process et l'utilisateur qui l'exécute
+    procs = {p.pid: p.info for p in psutil.process_iter([ 'name', 'username'])}
+
+    # On recherche le pid en fonction de la target donnée
+    for pid, info in procs.items():
+        if target in info['name']:
+            return pid
+        
+    return False
+
+print(search_proc("Notion"))
